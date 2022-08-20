94da5dc4fb09dbf948a0105d03a6ad8a279844f6
Saqif Abrar <abrars@uoguelph.ca>
2022-08-20 14:59:51 -0400

    added basic frontend routing

--- app_frontend/src/App.js
+++ app_frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard/dashboard";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				{/*<Navbar/>*/}
+				<Routes>
+					<Route path="/dashboard" element={<Dashboard />}></Route>
+					<Route path="*" element={<Dashboard />}></Route>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
