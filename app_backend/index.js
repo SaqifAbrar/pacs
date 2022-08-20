8ee7b1ba763247fbc991142307b8fc715d03e688
Saqif Abrar <abrars@uoguelph.ca>
2022-08-19 22:12:35 -0400

    updated basic backend structure

---
+++ app_backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+//const apiRoutes = require("./src/routes/routes");
+
+//require("dotenv").config();
+//app.use(express.json());
+
+// location routes
+//app.use("/api", apiRoutes);
+
+const port = process.env.PORT || 6000;
+
+app.listen(port, () => console.log(`pacs server is listening on port ${port}`));
