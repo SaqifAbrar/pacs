0ac91229df510f56e4a767b81332f673c91324d1
Shaiza Hashmi <soloyyz2000@yahoo.com>
2022-08-20 15:16:20 -0400

    prelim

---
+++ app_frontend/src/pages/dashboard/dashboard.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <h1>
+        BITHCES
+      </h1>
+      
+
+
+      
+    </div>
+  );
+}
+
+export default Dashboard;
