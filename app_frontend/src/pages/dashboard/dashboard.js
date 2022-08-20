99e52bf5252ac74a71387250266dc399c34dde59
Shaiza Hashmi <soloyyz2000@yahoo.com>
2022-08-20 15:39:17 -0400

    setup navbar component empty add

--- app_frontend/src/pages/dashboard/dashboard.js
+++ app_frontend/src/pages/dashboard/dashboard.js
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
-import './App.css';
-
-function Dashboard() {
-  return (
-    <div className="Dashboard">
-      
-      
-
-
-      
-    </div>
-  );
-}
-
-export default Dashboard;
-=======
 import React from "react";
 
 export default function Dashboard() {
-	return <div>Dashboard</div>;
+	return <div className="dashboard-page">
+        
+        
+
+
+    </div>;
 }
->>>>>>> 51cfff88f30e1256dfbf743b2163fe266645f922
