d6d566d371643fd69ec8a833db496628fc592942
Shaiza Hashmi <soloyyz2000@yahoo.com>
2022-08-20 16:18:59 -0400

    base divs and everything set up YAYYY

--- app_frontend/src/common/layout/navbar.js
+++ app_frontend/src/common/layout/navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Dashboard() {
 	return <div className="navbar-component">
         
-        
+        <h1> test navbar component</h1>
 
 
     </div>;
--- app_frontend/src/pages/dashboard/dashboard.js
+++ app_frontend/src/pages/dashboard/dashboard.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Navbar from "../../common/layout/navbar";
+import Sidebar from "./sidebar";
+
 
 export default function Dashboard() {
 	return <div className="dashboard-page">
-        
-        
+        {/* import navbar then a div? and import dashboard ig */}
+        <Navbar/>
+        <div className="dashboard-container">
+            <Sidebar/>
+            <div>
+                sample text to see where it renders
+            </div>
+        </div>
 
+   
 
     </div>;
 }
